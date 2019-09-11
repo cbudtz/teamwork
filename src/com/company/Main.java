abecd6bb5eaa7c623488fb403e3b3898756bc8ac
chbu <chbu@dtu.dk>
2019-09-11 10:04:58 +0200

    Spændende nyt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,7 @@ public class Main {
 
         System.out.println("Jeg pusher til Git :)");
         System.out.println("jeg har altså lavet ændringer :u");
+
+        System.out.println("Min nye og spændende kode!");
     }
 }
