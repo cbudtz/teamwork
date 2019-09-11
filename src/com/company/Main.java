b8f5d912606199469d163aa8b66b077be9e27f64
william <william.geismar@gmail.com>
2019-09-11 10:04:51 +0200

    Linjer fjernet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej William!");
         System.out.println("Det går fint");
-
-        System.out.println("Jeg pusher til Git :)");
-        System.out.println("jeg har altså lavet ændringer :u");
+        System.out.println("Nu har jeg fjernet et par linjer");
     }
 }
