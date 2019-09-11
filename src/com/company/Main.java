9943c5b02642e12fddc3771cb7d893555e17449b
william <william.geismar@gmail.com>
2019-09-11 09:59:21 +0200

    Man må ikke glemme commit message :'(

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej William!");
         System.out.println("Det går fint");
+
+        System.out.println("Jeg pusher til Git :)");
     }
 }
